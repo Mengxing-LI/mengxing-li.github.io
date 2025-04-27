99a5599e09e1475ad53b94a360e2b9eab779c854
Mengxing-LI <60501376+Mengxing-LI@users.noreply.github.com>
2025-04-27 13:18:14 +0000

    Deploying to gh-pages from @ Mengxing-LI/mengxing-li.github.io@fc4221f3ea097b69e91108d46f99f1e28cdaefc0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-mengxing-li",
+    title: "Mengxing LI",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
